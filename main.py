bd0f56cdf0aef1a0eca17d05a1396c488d0df5fe
mjd <mjdarvishi1374@gmail.com>
2024-02-01 20:49:08 +0100

    some chagnes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import os
 from utils.file_utility import read_cnf_file,read_cnf_text,read_file
 from solver.cdcl_solver import CDCL_SAT_SOLVER
-
+import time
 app = Flask(__name__)
 @app.route('/')
 def home():
@@ -22,28 +22,34 @@ def upload():
     # Process the file as needed
     # For example, you can save it or perform operations on its content
     file.save(os.path.join('public', file.filename))
+    start_time = time.time()
     num_var,num_claus,clauses=read_cnf_file('public/'+file.filename)
     sat_solver = CDCL_SAT_SOLVER()
     solution = sat_solver.solve(clauses, num_var)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
     if solution[0] != -1:
         print("Assignment verified")
         assn = solution[0][:]
         assn.sort(key=abs)
-        return render_template('result.html',result="\n".join(str(lit) for lit in assn),num_var=num_var,num_claus=num_claus)
+        return render_template('result.html',result="\n".join(str(lit) for lit in assn),num_var=num_var,num_claus=num_claus,time=elapsed_time)
     else:
         return render_template('result.html')
         
 @app.route('/solve', methods=['POST'])
 def salve():
     file = request.form['file']
+    start_time = time.time()
     num_var,num_claus,clauses=read_cnf_file('public/'+file)
     sat_solver = CDCL_SAT_SOLVER()
     solution = sat_solver.solve(clauses, num_var)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
     if solution[0] != -1:
         print("Assignment verified")
         assn = solution[0][:]
         assn.sort(key=abs)
-        return render_template('result.html',result="\n".join(str(lit) for lit in assn),num_var=num_var,num_claus=num_claus)
+        return render_template('result.html',result="\n".join(str(lit) for lit in assn),num_var=num_var,num_claus=num_claus,time=elapsed_time)
     else:
         return render_template('result.html')
         
@@ -53,16 +59,18 @@ def upload_text():
     if request.method == 'POST':
         # Text input logic
         cnf_text = request.form['text']
+        start_time = time.time()
         num_var, num_claus, clauses = read_cnf_text(cnf_text)
-
         # Rest of your code (as you provided)
         sat_solver = CDCL_SAT_SOLVER()
         solution = sat_solver.solve(clauses, num_var)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
         if solution[0] != -1:
             print("Assignment verified")
             assn = solution[0][:]
             assn.sort(key=abs)
-            return render_template('result.html',result="\n".join(str(lit) for lit in assn))
+            return render_template('result.html',result="\n".join(str(lit) for lit in assn),num_var=num_var,num_claus=num_claus,time=elapsed_time)
         else:
             return render_template('result.html')
     return 'not valid input'
