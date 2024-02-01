9ee65aa67132ae3ed230887ebd095aab4c400880
mjd <mjdarvishi1374@gmail.com>
2024-02-01 20:13:08 +0100

    fix host

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def read(name):
     text= read_file('public/'+name)
     return text
 if __name__ == '__main__':
-    app.run(debug=True,port=5002,host=0.0.0.0)
+    app.run(debug=True,port=5002,host='0.0.0.0')
 
 
 
